40d32d0739382eaf60ecf94f8372adc8908c3062
Petros Chatzipetrou <petros@axino.ai>
2025-02-16 11:40:33 +0100

    Add missing null returns

--- pkg/core/lib/models/multi_part_form_data_model_base.dart
+++ pkg/core/lib/models/multi_part_form_data_model_base.dart
@@ -4,9 +4,9 @@ abstract class MultiPartFormDataModelBase {
   String get filePath;
   String get fileMapKey;
   String get boundaryName => '--dio-boundary';
-  String? get filename;
-  Map<String, List<String>>? get headers;
+  String? get filename => null;
+  Map<String, List<String>>? get headers => null;
   ListFormat get listFormat => ListFormat.multi;
   bool get camelCaseContentDisposition => false;
-  DioMediaType? get contentType;
+  DioMediaType? get contentType => null;
 }
