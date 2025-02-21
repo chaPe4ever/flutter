d18a935c5bab0399dd90cad570fcab108c4c2858
Petros Chatzipetrou <petros@axino.ai>
2025-02-21 11:06:30 +0100

    Upgrade appBottomBar

--- pkg/shared_ui/lib/core/widgets/bars/app_bottom_button_bar.dart
+++ pkg/shared_ui/lib/core/widgets/bars/app_bottom_button_bar.dart
@@ -2,31 +2,46 @@ import 'package:core/core.dart';
 import 'package:flutter/material.dart';
 import 'package:shared_ui/shared_ui.dart';
 
-enum AppButtonBarType { single, double }
+enum AppButtonBarType { singlePrimary, singleSecondary, double }
 
 class AppBottomButtonBar extends StatelessWidget {
   const AppBottomButtonBar._(
     this._type, {
-    required this.labelPrimary,
-    required this.onPrimaryTap,
+    this.labelPrimary,
+    this.onPrimaryTap,
     this.labelSecondary,
     super.key,
     this.onSecondaryTap,
     this.padding,
   });
 
-  factory AppBottomButtonBar.single({
+  factory AppBottomButtonBar.singlePrimary({
     required String label,
     VoidCallback? onTap,
     EdgeInsetsGeometry? padding,
     Key? key,
   }) {
     return AppBottomButtonBar._(
-      AppButtonBarType.single,
+      AppButtonBarType.singlePrimary,
       key: key,
       padding: padding,
       labelPrimary: label,
       onPrimaryTap: onTap,
+    );
+  }
+
+  factory AppBottomButtonBar.singleSecondary({
+    required String label,
+    VoidCallback? onTap,
+    EdgeInsetsGeometry? padding,
+    Key? key,
+  }) {
+    return AppBottomButtonBar._(
+      AppButtonBarType.singleSecondary,
+      key: key,
+      padding: padding,
+      labelSecondary: label,
+      onSecondaryTap: onTap,
     );
   }
 
@@ -50,7 +65,7 @@ class AppBottomButtonBar extends StatelessWidget {
   }
 
   final AppButtonBarType _type;
-  final String labelPrimary;
+  final String? labelPrimary;
   final String? labelSecondary;
   final VoidCallback? onPrimaryTap;
   final VoidCallback? onSecondaryTap;
@@ -62,8 +77,8 @@ class AppBottomButtonBar extends StatelessWidget {
       color: context.colors.backgroundPrimary,
       padding: padding ?? AppPadding.buttonBar,
       child: switch (_type) {
-        AppButtonBarType.single => AppButton.primary(
-            label: labelPrimary,
+        AppButtonBarType.singlePrimary => AppButton.primary(
+            label: labelPrimary ?? '',
             onTap: onPrimaryTap,
           ),
         AppButtonBarType.double => SpacedRow(
@@ -78,11 +93,15 @@ class AppBottomButtonBar extends StatelessWidget {
               Expanded(
                 flex: 2,
                 child: AppButton.primary(
-                  label: labelPrimary,
+                  label: labelPrimary ?? '',
                   onTap: onPrimaryTap,
                 ),
               ),
             ],
+          ),
+        AppButtonBarType.singleSecondary => AppButton.secondary(
+            label: labelSecondary ?? '',
+            onTap: onSecondaryTap,
           ),
       },
     );
