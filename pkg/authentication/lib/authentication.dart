57b4fadf3bb4279ef5732d8cebc8bc5c58235d52
Petros Chatzipetrou <petros@axino.ai>
2025-04-10 00:38:24 +0200

    Export raw FirebaseAuth

--- pkg/authentication/lib/authentication.dart
+++ pkg/authentication/lib/authentication.dart
@@ -8,4 +8,10 @@ export 'package:authentication/implementations/implementations.dart';
 export 'package:authentication/pods/pods.dart';
 export 'package:authentication/utils/utils.dart';
 export 'package:firebase_auth/firebase_auth.dart'
-    show ActionCodeInfo, ActionCodeSettings, AuthCredential, Persistence, User;
+    show
+        ActionCodeInfo,
+        ActionCodeSettings,
+        AuthCredential,
+        FirebaseAuth,
+        Persistence,
+        User;
